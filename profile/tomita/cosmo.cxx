218b72cd4f771730e0c2841b9c0f193a03372216
Violentov Ivan <violentoff@gmail.com>
2015-02-20 22:40:15 +0300

    Add some new Context-Free Grammars (CFG)

--- profile/tomita/cosmo.cxx
+++ profile/tomita/cosmo.cxx
@@ -2,7 +2,9 @@
 
 #GRAMMAR_ROOT MainRule
 
-
 Fact     -> AnyWord<kwtype=fact>+;
+
+Object   -> Adj<gnc-agr[1]>* AnyWord<rt, gnc-agr[1], kwtype=object> Word<h-reg1> Noun<h-reg1>*;
   
 MainRule -> Fact interp(Fact.fact);
+MainRule -> Object interp(Object.object);
