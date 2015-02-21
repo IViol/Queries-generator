bbf33e38510c951c4f234ef5bc69ed95bffbe9e7
Ivan Violentov <violentoff@gmail.com>
2015-02-21 22:10:16 +0300

    New CFGs were added

--- profile/tomita/cosmo.cxx
+++ profile/tomita/cosmo.cxx
@@ -5,6 +5,9 @@
 Fact     -> AnyWord<kwtype=fact>+;
 
 Object   -> Adj<gnc-agr[1]>* AnyWord<rt, gnc-agr[1], kwtype=object> Word<h-reg1> Noun<h-reg1>*;
+
+IntellectD -> AnyWord<rt, gnc-agr[1], kwtype=inrellect_discovery> Word<h-reg1, gnc-agr[1]>+
   
 MainRule -> Fact interp(Fact.fact);
 MainRule -> Object interp(Object.object);
+MainRule -> IntellectD interp(IntellectD.inrellect_discovery);
